991221487381e3599b053e2f13a8845eb6ca0dc7
Yurii04UA <uyriparaka@gmail.com>
2022-04-07 16:28:17 +0300

    modal window

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 const overflow = document.querySelector('.over');
+// login n reg
 const btnLogin = document.querySelector('#login');
 const modalLogin = document.querySelector('.login');
-const btnClose = document.querySelector('.close');
+const btnCloseLog = document.querySelector('.close_log');
+const btnCloseReg = document.querySelector('.close_reg');
+
+const btnReg = document.querySelector('#registration') 
+const modalReg = document.querySelector('.registration')
 
 
-
+// modal
 btnLogin.addEventListener('click', () => {
    modalLogin.classList.toggle('show_modal');
    overflow.classList.toggle('hidden');
    document.querySelector('.circle').classList.toggle('show_modal');
 })
 
-btnClose.addEventListener('click', () => {
+btnReg.addEventListener('click', () => {
+   modalReg.classList.toggle('show_modal');
+   overflow.classList.toggle('hidden');
+   document.querySelector('.circle').classList.toggle('show_modal');
+})
+
+/// btn log n reg
+btnCloseLog.addEventListener('click', () => {
    modalLogin.classList.toggle('show_modal');
+   overflow.classList.toggle('hidden');
+   document.querySelector('.circle').classList.toggle('show_modal');
+})
+
+btnCloseReg.addEventListener('click', () => {
+   modalReg.classList.toggle('show_modal');
    overflow.classList.toggle('hidden');
    document.querySelector('.circle').classList.toggle('show_modal');
 })
