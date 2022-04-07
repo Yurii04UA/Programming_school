a475f79d01fc0d206ee83eb609affad5fe0e8b42
Yurii04UA <uyriparaka@gmail.com>
2022-04-07 18:37:26 +0300

    modal js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,49 @@
 const overflow = document.querySelector('.over');
-// login n reg
+//  login n reg
 const btnLogin = document.querySelector('#login');
 const modalLogin = document.querySelector('.login');
-const btnCloseLog = document.querySelector('.close_log');
-const btnCloseReg = document.querySelector('.close_reg');
 
-const btnReg = document.querySelector('#registration') 
-const modalReg = document.querySelector('.registration')
+const btnReg = document.querySelectorAll('#registration'); 
+const modalReg = document.querySelector('.registration');
+/// quiz
+const btnTest = document.querySelectorAll('.btn_test');
+const modalTest = document.querySelector('.quiz');
+//close
+const btnCloseModal = document.querySelectorAll('.close');
 
 
-// modal
+// show modal login reg n test
 btnLogin.addEventListener('click', () => {
    modalLogin.classList.toggle('show_modal');
    overflow.classList.toggle('hidden');
    document.querySelector('.circle').classList.toggle('show_modal');
 })
 
-btnReg.addEventListener('click', () => {
-   modalReg.classList.toggle('show_modal');
-   overflow.classList.toggle('hidden');
-   document.querySelector('.circle').classList.toggle('show_modal');
+btnReg.forEach(e => {
+   e.addEventListener('click', () => {
+         modalReg.classList.toggle('show_modal');
+         overflow.classList.toggle('hidden');
+         document.querySelector('.circle').classList.toggle('show_modal');
+      })
 })
 
-/// btn log n reg
-btnCloseLog.addEventListener('click', () => {
-   modalLogin.classList.toggle('show_modal');
-   overflow.classList.toggle('hidden');
-   document.querySelector('.circle').classList.toggle('show_modal');
+btnTest.forEach(e => {
+   e.addEventListener('click', () => {
+      modalTest.classList.toggle('show_modal');
+      overflow.classList.toggle('hidden');
+      document.querySelector('.circle').classList.toggle('show_modal');
+   })
 })
 
-btnCloseReg.addEventListener('click', () => {
-   modalReg.classList.toggle('show_modal');
-   overflow.classList.toggle('hidden');
-   document.querySelector('.circle').classList.toggle('show_modal');
+// close modal
+btnCloseModal.forEach(e =>{
+   e.addEventListener('click', () => {
+         modalLogin.classList.remove('show_modal');
+         modalReg.classList.remove('show_modal');
+         modalTest.classList.remove('show_modal');
+         overflow.classList.remove('hidden');
+         document.querySelector('.circle').classList.remove('show_modal');
+      })
 })
+
+
